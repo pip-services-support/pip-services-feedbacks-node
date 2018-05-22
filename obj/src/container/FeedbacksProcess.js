15110acb4585919b7e3713929e1ec7df5bcbf798
pipdeveloper <pipdevs@gmail.com>
2018-05-22 16:57:53 +0000

    project build by Travis CI [skip ci]

--- obj/src/container/FeedbacksProcess.js
+++ obj/src/container/FeedbacksProcess.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
+const pip_services_net_node_1 = require("pip-services-net-node");
+const pip_services_oss_node_1 = require("pip-services-oss-node");
 const FeedbacksServiceFactory_1 = require("../build/FeedbacksServiceFactory");
 class FeedbacksProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("feedbacks", "User feedbacks microservice");
         this._factories.add(new FeedbacksServiceFactory_1.FeedbacksServiceFactory);
+        this._factories.add(new pip_services_net_node_1.DefaultNetFactory);
+        this._factories.add(new pip_services_oss_node_1.DefaultOssFactory);
     }
 }
 exports.FeedbacksProcess = FeedbacksProcess;

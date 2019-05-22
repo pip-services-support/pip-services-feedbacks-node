2bf22ed8d396eb6753fd79293939103f4fbb3804
pipdeveloper <pipdevs@gmail.com>
2019-05-22 11:46:38 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/FeedbacksMongoDbPersistence.d.ts
+++ obj/src/persistence/FeedbacksMongoDbPersistence.d.ts
@@ -5,6 +5,6 @@ import { FeedbackV1 } from '../data/version1/FeedbackV1';
 import { IFeedbacksPersistence } from './IFeedbacksPersistence';
 export declare class FeedbacksMongoDbPersistence extends IdentifiableMongoosePersistence<FeedbackV1, string> implements IFeedbacksPersistence {
     constructor();
-    private composeFilter;
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: any): void;
 }

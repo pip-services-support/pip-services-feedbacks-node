43d0e85a2c9444da5e3a30258b2c4aefb541e3f9
Dmitriy Krayniy <krdima92@gmail.com>
2020-11-30 14:45:15 +0300

    update ci pipeline and moved to github actions

--- clean.ps1
+++ clean.ps1
@@ -2,9 +2,10 @@
 
 # Get component data and set necessary variables
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-$buildImage="$($component.registry)/$($component.name):$($component.version)-$($env:TRAVIS_BUILD_NUMBER)-build"
-$testImage="$($component.registry)/$($component.name):$($component.version)-$($env:TRAVIS_BUILD_NUMBER)-test"
-$rcImage="$($component.registry)/$($component.name):$($component.version)-$($env:TRAVIS_BUILD_NUMBER)-rc"
+$buildImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
+$testImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
+$rcImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-rc"
+$latestImage="$($component.registry)/$($component.name):latest"
 
 # Clean up build directories
 Get-ChildItem -Path "." -Include "obj" -Recurse | foreach($_) { Remove-Item -Force -Recurse $_.FullName }
@@ -14,7 +15,13 @@ Get-ChildItem -Path "." -Include "node_modules" -Recurse | foreach($_) { Remove-
 docker rmi $buildImage --force
 docker rmi $testImage --force
 docker rmi $rcImage --force
+docker rmi $latestImage --force
 docker image prune --force
+docker rmi -f $(docker images -f "dangling=true" -q) # remove build container if build fails
 
 # Remove existed containers
-docker ps -a | Select-String -Pattern "Exit" | foreach($_) { docker rm $_.ToString().Split(" ")[0] }
+$exitedContainers = docker ps -a | Select-String -Pattern "Exit"
+foreach($c in $exitedContainers) { docker rm $c.ToString().Split(" ")[0] }
+
+# Remove unused volumes
+docker volume rm -f $(docker volume ls -f "dangling=true")
--- package.ps1
+++ package.ps1
@@ -5,13 +5,21 @@ $ErrorActionPreference = "Stop"
 
 # Get component data and set necessary variables
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-$stageImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-rc"
+$rcImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-rc"
+$latestImage="$($component.registry)/$($component.name):latest"
 
 # Build docker image
-docker build -f docker/Dockerfile -t $stageImage .
+docker build -f docker/Dockerfile -t $rcImage -t $latestImage .
 
 # Set environment variables
-$env:IMAGE = $stageImage
+$env:IMAGE = $rcImage
+
+# Set docker machine ip (on windows not localhost)
+if ($env:DOCKER_IP -ne $null) {
+    $dockerMachineIp = $env:DOCKER_IP
+} else {
+    $dockerMachineIp = "localhost"
+}
 
 try {
     # Workaround to remove dangling images
@@ -21,8 +29,8 @@ try {
 
     # Test using curl
     Start-Sleep -Seconds 10
-    Invoke-WebRequest -Uri http://localhost:8080/heartbeat
-    #Invoke-WebRequest -Uri http://localhost:8080/v1/feedbacks/get_feedbacks
+    Invoke-WebRequest -Uri "http://$($dockerMachineIp):8080/heartbeat"
+    #Invoke-WebRequest -Uri http://$($dockerMachineIp):8080/v1/feedbacks/get_feedbacks
 
     Write-Host "The container was successfully built."
 } finally {
